8c24f474b897d12658d828ab1c7e9a2806793415
muzammaldev-780 <muzammal.murtaza@devsinc.com>
2020-10-14 10:53:04 -0700

    videos added

--- app/services/test.rb
+++ app/services/test.rb
@@ -7,12 +7,11 @@ class Test
 	 	}
 		 YoutubeDL.download(url, options)
 	end
-
-	def self.download_with_lib_video(url)
-		options = {
-			format: 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-			output: 'abc'
-		}
-		system("youtube-dl 'https://www.youtube.com/watch?v=gRhmbfwCdqo'")
-   end
+# 	def self.download_with_lib_video(url)
+# 		options = {
+# 			format: 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+# 			output: 'abc'
+# 		}
+# 		system("youtube-dl 'https://www.youtube.com/watch?v=gRhmbfwCdqo'")
+#    end
 end
